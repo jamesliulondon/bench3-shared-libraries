5041d17f18fe455d02727f2eb80fcb8e005c8439
James Liu <jamesliulondon@gmail.comQ>
2021-03-16 11:35:42 +0000

    import fq class

--- vars/loader.groovy
+++ vars/loader.groovy
@@ -1,6 +1,6 @@
-import com.accprdops.bench3.global.*
+import com.accprdops.bench3.global.YamlReaderExample
 
 def call(String name = 'human') {
     echo "Hello, ${name}."
-    String yre = new YamlReaderExample()
+//    String yre = new YamlReaderExample()
 }
