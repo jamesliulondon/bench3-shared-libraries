584cb96e2d2a1bdd293c41be6b1a7fa62a4e6075
James Liu <jamesliulondon@gmail.comQ>
2021-03-16 10:51:21 +0000

    instatiate java

--- vars/loader.groovy
+++ vars/loader.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
 import com.accordops.bench1.global.YamlReaderExample
 def call(String name = 'human') {
+    YamlReaderExample yre = new YamlReaderExample()
     echo "Hello, ${name}."
 }
