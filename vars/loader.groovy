f390244d68a56e989f7ecc32581db2265b020d7b
James Liu <jamesliulondon@gmail.comQ>
2021-03-16 10:52:20 +0000

    println yre

--- vars/loader.groovy
+++ vars/loader.groovy
@@ -3,4 +3,5 @@ import com.accordops.bench1.global.YamlReaderExample
 def call(String name = 'human') {
     YamlReaderExample yre = new YamlReaderExample()
     echo "Hello, ${name}."
+    println(yre)
 }
